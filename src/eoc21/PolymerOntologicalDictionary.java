d5c55fd2da272cec8c00ec11dd211b959f8b03c9
Ed Cannon <eoc21@cam.ac.uk>
2010-03-03 11:46:48 +0000

    Using LingPipe and Rome API to extract RSS feeds from the BBC news and polymer journals to generate a training and test set automatically for text classification. The classification will then be assisted by our PolymerOntologicalDictionary class.

---
+++ src/eoc21/PolymerOntologicalDictionary.java
@@ -0,0 +1,11 @@
+package eoc21;
+/**
+ * This class extracts all the OWL classes from the polymer ontologies we have
+ * created: properties, techniques, conditions and units and adds the terms to
+ * a dictionary that may assist text classification.
+ * @author ed
+ *
+ */
+public class PolymerOntologicalDictionary {
+
+}
